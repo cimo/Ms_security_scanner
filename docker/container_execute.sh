88092f26f98ee0b4be6c38677b00b80d48cf3ede
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-08-18 17:40:34 +0900

    update

--- docker/container_execute.sh
+++ docker/container_execute.sh
@@ -15,10 +15,12 @@ parameter2="${p2}"
 
 echo -e "\nCopying from volume..."
 
+projectName="cimo"
+
 docker run --rm \
 -e HOST_UID="$(id -u)" \
 -e HOST_GID="$(id -g)" \
--v "cimo_${parameter1}_ms_cronjob-volume:/home/source/:ro" \
+-v "${projectName}_${parameter1}_ms_cronjob-volume:/home/source/:ro" \
 -v "$(pwd)/certificate/:/home/target/" \
 alpine sh -c 'cp -r "/home/source/"* "/home/target/" && chown -R "$HOST_UID:$HOST_GID" "/home/target/" && chmod -R u+rwX,go+rX "/home/target/"'
 
@@ -30,5 +32,7 @@ then
     docker compose -f "docker-compose.yaml" --env-file "./env/${parameter1}.env" --env-file "./env/${parameter1}.secret.env" up --detach --pull always
 elif [ "${parameter2}" = "up" ]
 then
-    docker compose -f "docker-compose.yaml" --env-file "./env/${parameter1}.env" --env-file "./env/${parameter1}.secret.env" up --detach --pull always
+    docker compose -f "docker-compose.yaml" --env-file "./env/${parameter1}.env" --env-file "./env/${parameter1}.secret.env" up --detach --pull always --wait &&
+    docker compose -f "docker-compose.yaml" --env-file "./env/${parameter1}.env" --env-file "./env/${parameter1}.secret.env" exec -u root -T "${projectName}_ms_security_scan" sh -c 'cp -a "${PATH_ROOT}certificate/." "/usr/local/share/ca-certificates/"' &&
+    docker compose -f "docker-compose.yaml" --env-file "./env/${parameter1}.env" --env-file "./env/${parameter1}.secret.env" exec -u root -T "${projectName}_ms_security_scan" update-ca-certificates
 fi
