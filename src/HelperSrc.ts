6917fd6b8a861a532b551fd06d657b77dc540314
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-08-19 16:51:13 +0900

    update

--- src/HelperSrc.ts
+++ src/HelperSrc.ts
@@ -186,7 +186,7 @@ export const jsonCheck = (value: string): boolean => {
     }
 };
 
-export const fileDetail = (value: string, buffer?: Uint8Array, isOnlyByte = true): modelHelperSrc.IfileDetail => {
+export const fileDetail = async (value: string, buffer?: Uint8Array, isOnlyByte = true): Promise<modelHelperSrc.IfileDetail> => {
     let resultObject = {} as modelHelperSrc.IfileDetail;
 
     if (!value) {
@@ -196,14 +196,22 @@ export const fileDetail = (value: string, buffer?: Uint8Array, isOnlyByte = true
     const fileNameWithExtension = value.includes("/") ? value.split("/").pop()! : value;
     const baseName = fileNameWithExtension.trim().replace(/\.[^/.]+$/, "");
 
-    if (value.includes("/") && Fs.existsSync(value)) {
-        const stat = Fs.statSync(value);
+    if (value.includes("/")) {
+        resultObject = await new Promise<modelHelperSrc.IfileDetail>((resolve) => {
+            Fs.stat(value, (error, stats) => {
+                if (error) {
+                    resolve(resultObject);
 
-        resultObject = {
-            ...resultObject,
-            size: fileSize(stat.size, isOnlyByte),
-            dateModified: localeFormat(stat.mtime) || ""
-        };
+                    return;
+                }
+
+                resolve({
+                    ...resultObject,
+                    size: fileSize(stats.size, isOnlyByte),
+                    dateModified: localeFormat(stats.mtime) || ""
+                });
+            });
+        });
     }
 
     const signatureList: modelHelperSrc.IfileDetailSignature[] = [
@@ -605,7 +613,7 @@ export const findPathFileRecursive = (path: string, extension: string): Promise<
 export const findPathDirnameRecursive = async (path: string, fileName: string): Promise<string> => {
     let result = "";
 
-    const detail = fileDetail(fileName);
+    const detail = await fileDetail(fileName);
 
     const pathFileList = await findPathFileRecursive(path, detail.extension);
 
